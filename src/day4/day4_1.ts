0709c347f5d731a60481e7953c55494d20671629
Oscar Fredriksson <oscar@famfredriksson.com>
2022-12-04 07:13:39 +0100

    refactor

--- src/day4/day4_1.ts
+++ src/day4/day4_1.ts
@@ -3,17 +3,14 @@ import { readFileSync } from "fs";
 const part1 = readFileSync(__dirname + "/input.txt", "utf-8")
   .split("\n")
   .map((row) =>
-    row
-      .split(",")
-      .flatMap((row) => row.split("-"))
-      .map((value) => parseInt(value))
+    row.split(",").map((row) => row.split("-").map((value) => parseInt(value)))
   )
   .reduce(
     (count, pair) =>
-      (pair[0] >= pair[2] && pair[1] <= pair[3]) ||
-      (pair[2] >= pair[0] && pair[3] <= pair[1])
+      (pair[0][0] >= pair[1][0] && pair[0][1] <= pair[1][1]) ||
+      (pair[0][0] <= pair[1][0] && pair[0][1] >= pair[1][1])
         ? count + 1
         : count,
     0
   );
-console.log(part1);
+console.log(part1); //528
--- src/day4/day4_2.ts
+++ src/day4/day4_2.ts
@@ -3,21 +3,12 @@ import { readFileSync } from "fs";
 const part2 = readFileSync(__dirname + "/input.txt", "utf-8")
   .split("\n")
   .map((row) =>
-    row
-      .split(",")
-      .flatMap((row) => row.split("-"))
-      .map((value) => parseInt(value))
+    row.split(",").map((row) => row.split("-").map((value) => parseInt(value)))
   )
   .reduce(
     (count, pair) =>
-      (pair[0] < pair[2] &&
-        pair[1] < pair[2] &&
-        pair[0] < pair[3] &&
-        pair[1] < pair[3]) ||
-      (pair[0] > pair[2] &&
-        pair[1] > pair[2] &&
-        pair[0] > pair[3] &&
-        pair[1] > pair[3])
+      pair[0].every((elf1) => pair[1].every((elf2) => elf1 < elf2)) ||
+      pair[0].every((elf1) => pair[1].every((elf2) => elf1 > elf2))
         ? count
         : count + 1,
     0
